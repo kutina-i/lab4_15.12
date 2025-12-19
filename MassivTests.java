82502106565bca3b9c9fe9b3a4c922707136505d
kutina-i <93332765+kutina-i@users.noreply.github.com>
2025-12-19 15:57:24 +0600

    Create MassivTests.java

---
+++ MassivTests.java
@@ -0,0 +1,70 @@
+import org.junit.Test;
+
+public class MassivTests {
+
+    @Test
+    public void testTask1(){
+        Massiv task = new Massiv();
+        task.Task1();
+    }
+
+    @Test
+    public void testTask2(){
+        Massiv task = new Massiv();
+        task.Task2();
+    }
+
+    @Test
+    public void testTask3(){
+        Massiv task = new Massiv();
+        task.Task3();
+    }
+
+    @Test
+    public void testTask4(){
+        Massiv task = new Massiv();
+        task.Task4();
+    }
+
+    @Test
+    public void testTask5(){
+        Massiv task = new Massiv();
+        task.Task5();
+    }
+
+    @Test
+    public void testTask6(){
+        Massiv task = new Massiv();
+        task.Task6();
+    }
+
+    @Test
+    public void testTask7(){
+        Massiv task = new Massiv();
+        task.Task7();
+    }
+
+    @Test
+    public void testTask8(){
+        Massiv task = new Massiv();
+        task.Task8();
+    }
+
+    @Test
+    public void testTask9(){
+        Massiv task = new Massiv();
+        task.Task9();
+    }
+
+    @Test
+    public void testTask10(){
+        Massiv task = new Massiv();
+        task.Task10();
+    }
+
+    @Test
+    public void testTask11(){
+        Massiv task = new Massiv();
+        task.Task11();
+    }
+}
